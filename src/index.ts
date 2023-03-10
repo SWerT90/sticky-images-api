0cbf5f33e453bb4b73b24e9fa7c7bc86144426a4
Miguel <castorcillo90@gmail.com>
2023-03-10 21:11:08 +0100

    change dev script to use ts-node-env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import colors from 'picocolors'
 
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
-  console.log(`⚡App listening on port ${port}`)
+  console.log(colors.green(`⚡ App listening on port ${port}`))
 })
